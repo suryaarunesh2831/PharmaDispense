7e471e111be347b247a09ca8f2c3da818318af79
K <2479479@cognizant.com>
2026-03-10 15:32:28 +0530

    User table Updated

--- src/main/java/com/example/pharmacy/model/User.java
+++ src/main/java/com/example/pharmacy/model/User.java
@@ -2,7 +2,10 @@ package com.example.pharmacy.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Data;
+
 import java.util.*;
+@Data
 @Entity
 @Table(name = "`User`")   // IMPORTANT: escape reserved table name
 public class User {
@@ -16,62 +19,14 @@ public class User {
     private String email;
     private String phone;
 
-    public List<AuditLog> getAuditlog() {
-        return auditlog;
-    }
-
-    public void setAuditlog(List<AuditLog> auditlog) {
-        this.auditlog = auditlog;
-    }
-
     @JsonIgnore
     @OneToMany(mappedBy = "user",cascade=CascadeType.ALL)
     private List<AuditLog>auditlog;
     public User() {}
 
-    public int getUserID() {
-        return userID;
-    }
-    public void setUserID(int userID) {
-        this.userID = userID;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getRole() {
-        return role;
-    }
-    public void setRole(String role) {
-        this.role = role;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPhone() {
-        return phone;
-    }
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
     @OneToMany(mappedBy ="user" )
     private List<Prescription> prescriptions=new ArrayList<>();
-    //
-//    @ManyToMany
-//            @JoinTable(
-//                    name="UserNotification table",
-//                    joinColumns = @JoinColumn(name="UserID"),
-//                    inverseJoinColumns = @JoinColumn(name="NotificationId")
-//
-//            )
-//    List<Notification>notificationList=new ArrayList<>();
-//
+
     @OneToMany(mappedBy = "user")
     List<Verification>verificationList=new ArrayList<>();
 
